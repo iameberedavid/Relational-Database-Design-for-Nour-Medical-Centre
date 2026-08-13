8a59cb98a19ea5f463d17f5b68979b8372723b99
Chidiebere David Ogbonna <chidieberedavid326@gmail.com>
2026-08-13 15:19:01 +0100

    Update 01_create_database.sql

--- database/01_create_database.sql
+++ database/01_create_database.sql
@@ -1,1 +1,15 @@
+-- ============================================================
+-- Nour Medical Centre
+-- Relational Database Design
+-- File: 01_create_database.sql
+-- Purpose: Create the project database
+-- ============================================================
+
+CREATE DATABASE IF NOT EXISTS nour_medical_centre;
+
+-- "IF NOT EXISTS" prevents MySQL from throwing an error if you run the script again after the database has already been created.
+
+USE nour_medical_centre;
+
+-- "USE" tells MySQL: "From this point forward, execute my SQL statements inside the Nour Medical Centre database."
 
